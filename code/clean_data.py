ee426f121a2191efb7cdfa502efa5bac25b4f975
Vighnesh Avadhanam <vighnesh.avadhanam@utexas.edu>
2025-05-02 22:28:50 -0500

    Adding cleaned data converted to binary

---
+++ code/clean_data.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+input_path = os.path.join(base_dir, "../data/train_data.csv")
+output_path = os.path.join(base_dir, "../data/train_data_clean.csv")
+
+
+df = pd.read_csv(input_path, sep=";", header=0, names=["comment", "isHate"], encoding="utf-8")
+df['isHate'] = (df['isHate'].astype(float) >= 0.25).astype(int)
+df.to_csv(output_path, index=False)
