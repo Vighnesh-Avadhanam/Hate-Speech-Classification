03f13239405ba12daf773498e961249a050e1672
Vighnesh Avadhanam <vighnesh.avadhanam@utexas.edu>
2025-05-05 01:38:05 -0500

    Update logisticc hate bert

--- code/logistic_hate_bert.py
+++ code/logistic_hate_bert.py
@@ -118,8 +118,8 @@ class LogisticHateBERT(BaseEstimator, ClassifierMixin):
         plt.close()
 
 
-train_df = pd.read_csv("C:/Users/Admin/Desktop/ML/eco395m_ml_final/data/train_data.csv")
-test_df = pd.read_csv("C:/Users/Admin/Desktop/ML/eco395m_ml_final/data/test_data.csv", sep=";")
+train_df = pd.read_csv("../data/train_data.csv")
+test_df = pd.read_csv("../data/test_data.csv", sep=";")
 
 train_texts = train_df["text"].tolist()
 train_labels = train_df["label"].tolist()
